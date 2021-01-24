coder/coder-v1-cli

commit daa3f7a8e00a5575f35b6ca3bc1dbd757c275fdc

fix: create login shell with coder sh (#224)
When creating a shell, we should use "exec -a" to set argv[0]
to the basename of the command being run, prefixing a hyphen.
This is how su --login creates login shells: it runs /bin/bash
as "-bash", causing bash to execute .bash_profile, etc.

We change the use of awk to cut, since cut is part of coreutils
and thus more likely to be installed than gawk. We also pass the
uid (from "id -u") instead of username to getent.

internal/cmd/shell.go
@@ -85,7 +85,9 @@ func shell(cmd *cobra.Command, cmdArgs []string) error {
 		args = append(args, strings.Join(cmdArgs[1:], " "))
 	} else {
 		// Bring user into shell if no command is specified.
-		args = append(args, "exec $(getent passwd $(whoami) | awk -F: '{ print $7 }')")
+		shell := "$(getent passwd $(id -u) | cut -d: -f 7)"
+		name := "-$(basename " + shell + ")"
+		args = append(args, fmt.Sprintf("exec -a %q %q", name, shell))
 	}
 
 	envName := cmdArgs[0]

fossabot/coder-cli

commit e34150ac897f07c64e30dfc3a1740766f4ecfece

fix: Force bash for non-standard flags to exec
This addresses a regression introduced by #224. POSIX sh does not
define arguments to the exec built-in, resulting in an error when
using 'exec -a' to set the process name (argv[0]).

This change executes /bin/bash instead of sh, and also changes
to use 'exec -l', which automatically handles the hyphen prefix
required to trigger login shell behavior.

This also explicitly runs /bin/sh, aligning the behavior of
"coder sh" and the frontend Terminal application.

internal/cmd/shell.go
@@ -79,15 +79,20 @@ coder sh front-end-dev cat ~/config.json`,
 
 func shell(cmd *cobra.Command, cmdArgs []string) error {
 	ctx := cmd.Context()
-	command := "sh"
-	args := []string{"-c"}
+	var command string
+	var args []string
 	if len(cmdArgs) > 1 {
+		command = "/bin/sh"
+		args = []string{"-c"}
 		args = append(args, strings.Join(cmdArgs[1:], " "))
 	} else {
 		// Bring user into shell if no command is specified.
 		shell := "$(getent passwd $(id -u) | cut -d: -f 7)"
-		name := "-$(basename " + shell + ")"
-		args = append(args, fmt.Sprintf("exec -a %q %q", name, shell))
+
+		// force bash for the '-l' flag to the exec built-in
+		command = "/bin/bash"
+		args = []string{"-c"}
+		args = append(args, fmt.Sprintf("exec -l %q", shell))
 	}
 
 	envName := cmdArgs[0]